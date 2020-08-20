055027cd6dec72ff2f502ae8dd3da4621d1bf816
antemaricic5 <46193415+antemaricic5@users.noreply.github.com>
2020-08-20 16:25:26 +0200

    fixenv

--- env.php
+++ env.php
@@ -3,10 +3,10 @@
 namespace db;
 
 class DbConfig{
-    const HOST="localhost";
-    const USER = "root";
-    const PASS = "";
-    const DB ="web programiranje";
+    const HOST="eu-cdbr-west-03.cleardb.net";
+    const USER = "ba81fe7a2a63aa";
+    const PASS = "84ae211e";
+    const DB ="heroku_0de96f546acd4ba";
 }
 
 ?>
